ca70083bce55611517e119af8da511f98f3cdbc1
root <kalebabiy22>
2021-11-18 11:15:01 -0800

    edited

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-from sys import argv
-
-
-def principal():
-    print('{} argument'.format(len(argv) - 1), end='')
-    if len(argv) == 1:
-        print('s.')
-    elif len(argv) == 2:
-        print(':')
-    else:
-        print('s:')
-    for i in range(1, len(argv)):
-        print('{}: {}'.format(i, argv[i]))
 if __name__ == "__main__":
-    principal()
+    from sys import argv
+    arguments = len(argv) - 1
 
+    if arguments is 0:
+        print("{:d} arguments.".format(arguments))
+    elif arguments is 1:
+        print("{:d} argument:".format(arguments))
+        print("{:d}: {}".format(1, str(argv[1])))
+    else:
+        print("{:d} arguments:".format(arguments))
+        for i in range(1, len(argv)):
+            print("{:d}: {}".format(i, argv[i]))
